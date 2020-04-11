e757539a4a2a30813224eb72c40b41c9caaf4a71
TAT527 <47490150+TAT527@users.noreply.github.com>
2020-04-11 14:11:29 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,83 @@
+from flask import Flask, request, abort
+from unmo import Unmo
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+import os
+import gc
+ 
+app = Flask(__name__)
+ 
+#環境変数取得
+# LINE Developersで設定されているアクセストークンとChannel Secretをを取得し、設定します。
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+ 
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+ 
+ 
+## 1 ##
+#Webhookからのリクエストをチェックします。
+@app.route("/callback", methods=['POST'])
+def callback():
+    # リクエストヘッダーから署名検証のための値を取得します。
+    signature = request.headers['X-Line-Signature']
+ 
+    # リクエストボディを取得します。
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+ 
+    # handle webhook body
+# 署名を検証し、問題なければhandleに定義されている関数を呼び出す。
+    try:
+        handler.handle(body, signature)
+# 署名検証で失敗した場合、例外を出す。
+    except InvalidSignatureError:
+        abort(400)
+# handleの処理を終えればOK
+    return 'OK'
+
+
+def build_prompt(unmo):
+    """AIインスタンスを取り、AIとResponderの名前を整形して返す"""
+    return '{name}:{responder}> '.format(name=unmo.name,
+                                         responder=unmo.responder_name)
+
+
+
+## 2 ##
+###############################################
+#LINEのメッセージの取得と返信内容の設定(オウム返し)
+###############################################
+ 
+#LINEでMessageEvent（普通のメッセージを送信された場合）が起こった場合に、
+#def以下の関数を実行します。
+#reply_messageの第一引数のevent.reply_tokenは、イベントの応答に用いるトークンです。 
+#第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
+ 
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    #if event.reply_token == "00000000000000000000000000000000":
+        #return
+    if __name__ == '__main__':
+    	Lala = Unmo('Lala')
+    #while True:
+        #text = TextMessage
+    response = Lala.dialogue(text=event.message.text)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=response)) #ここでオウム返しのメッセージを返します。
+    gc.collect()
+ 
+# ポート番号の設定
+if __name__ == "__main__":
+#    app.run()
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
